12f3446bcd38509690e74071b3aaf2297c2ebb4b
Leopold Tal G <leopold.tal.dg@gmail.com>
2021-11-03 21:25:33 +0100

    transform standard input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use porcus;
+use std::io;
 
 fn main() {
-	println!("{}", porcus::to_pig_latin(String::from("This is a test.")));
+	loop {
+		let mut input = String::new();
+		let read_size = io::stdin()
+			.read_line(&mut input)
+			.expect("Failed to read line");
+
+		if read_size == 0 {
+			break;
+		}
+
+		let as_pig_latin = porcus::to_pig_latin(input);
+		print!("{}", as_pig_latin);
+	}
 }
